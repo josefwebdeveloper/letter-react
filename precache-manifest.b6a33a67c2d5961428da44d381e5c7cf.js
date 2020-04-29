3b7903d15851de819300f4c85924c3e283a0d2c8
josephs <josef2011@gmail.com>
2020-04-29 19:20:00 +0300

    Updates

---
+++ precache-manifest.b6a33a67c2d5961428da44d381e5c7cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f14c83cd1d98f765256ea665b3189491",
+    "url": "/letter-react/index.html"
+  },
+  {
+    "revision": "68c6035ada081a1882ef",
+    "url": "/letter-react/static/css/main.424a5570.chunk.css"
+  },
+  {
+    "revision": "a165dd8986e93b296df8",
+    "url": "/letter-react/static/js/2.137956e9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/letter-react/static/js/2.137956e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68c6035ada081a1882ef",
+    "url": "/letter-react/static/js/main.8f7c5264.chunk.js"
+  },
+  {
+    "revision": "349b335e112405339642",
+    "url": "/letter-react/static/js/runtime-main.f4f749f4.js"
+  }
+]);
