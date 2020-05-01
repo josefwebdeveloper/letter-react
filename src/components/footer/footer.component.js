3c9a779d1fe127694b5217ff5204be4e675f03fe
josephs <josef2011@gmail.com>
2020-05-01 13:14:12 +0300

    kkk

--- src/components/footer/footer.component.js
+++ src/components/footer/footer.component.js
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <div className='footer border border-1 note'>
             <div><a href="https://ko-fi.com/buy_me_cofee">Buy me a coffee on Ko-fi </a>
-                · Support me on Patreon
+                <a href="https://www.patreon.com/support_startup">·Support me on Patreon</a>
             </div>
         </div>
     );
